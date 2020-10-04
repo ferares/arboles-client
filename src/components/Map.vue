5172e6302661470f1d5b3beb6deb213162688ea0
Ferares <fermin@ares.uy>
2020-10-04 17:02:21 -0300

    Add Leaflet map

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <l-map :zoom="zoom" :center="center" :options="options">
+    <l-tile-layer :url="url"></l-tile-layer>
+  </l-map>
+</template>
+
+<script>
+// import L from "leaflet";
+// import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { LMap, LTileLayer } from "vue2-leaflet";
+
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer
+    // LMarker
+  },
+  data: function() {
+    return {
+      zoom: 12,
+      center: [-34.618, -58.44],
+      options: {
+        maxZoom: 21,
+        minZoom: 5
+      },
+      url:
+        "https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
+    };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,84 +1,86 @@
 <template>
-  <div class="container-fluid">
-    <aside class="col-12 col-md-4 col-lg-3 py-4">
-      <a class="d-flex flex-row-reverse align-items-center mb-4" href="/">
-        <h1 class="main-title">
-          Arbolado Urbano
-          <small class="text-secondary">
-            Internacional
-          </small>
-        </h1>
-        <img class="img-fluid" src="../assets/logo-blanco.png" alt="" />
-      </a>
+  <div class="container-fluid h-100">
+    <div class="row h-100">
+      <aside class="col-12 col-md-4 col-lg-3 py-4">
+        <a class="d-flex flex-row-reverse align-items-center mb-4" href="/">
+          <h1 class="main-title">
+            Arbolado Urbano
+            <small class="text-secondary">
+              Internacional
+            </small>
+          </h1>
+          <img class="img-fluid" src="../assets/logo-blanco.png" alt="" />
+        </a>
 
-      <Form />
+        <Form />
 
-      <div class="row mt-4">
-        <div class="col-12 col-sm-3 col-md-6">
+        <div class="row mt-4">
+          <div class="col-12 col-sm-3 col-md-6">
+            <a
+              class="btn btn-light btn-small btn-block"
+              href="https://www.facebook.com/arboladomapa"
+              target="_blank"
+            >
+              <Icon :icon="['fab', 'facebook']" />
+              Seguinos
+            </a>
+          </div>
+          <div class="col-12 col-sm-3 col-md-6">
+            <button
+              class="btn btn-light btn-small btn-block"
+              @click="$bvModal.show('modal-about')"
+            >
+              Sobre el mapa
+            </button>
+          </div>
+        </div>
+
+        <Adsense :ad-client="adsenseClient" :ad-slot="adsenseSlot"></Adsense>
+
+        <div class="row text-center mt-4">
+          <p class="col-12 col-sm-3 col-md-12 text-muted">
+            Este mapa cuenta con<br />
+            la valiosa colaboración de:
+          </p>
           <a
-            class="btn btn-light btn-small btn-block"
-            href="https://www.facebook.com/arboladomapa"
+            class="col-6 col-sm-3 col-md-6 col-lg-4"
+            href="https://www.facebook.com/LaCiudadNosRegalaSabores"
             target="_blank"
           >
-            <Icon :icon="['fab', 'facebook']" />
-            Seguinos
+            <img
+              class="img-fluid"
+              src="../assets/colaborador-lcnrs.png"
+              alt="La ciudad nos regala sabores"
+            />
+          </a>
+          <a
+            class="col-6 col-sm-3 col-md-6 col-lg-4"
+            href="https://www.facebook.com/elrenacerdelalaguna/"
+            target="_blank"
+          >
+            <img
+              class="img-fluid"
+              src="../assets/colaborador-laguna-fvet.png"
+              alt="El Renacer de la Laguna - FVET - UBA"
+            />
+          </a>
+          <a
+            class="col-12 col-sm-3 col-md-12 col-lg-4"
+            href="https://www.facebook.com/AsociacionRiberaNorte/"
+            target="_blank"
+          >
+            <img
+              class="img-fluid"
+              src="../assets/colaborador-arn.png"
+              alt="Asociación Ribera Norte"
+            />
           </a>
         </div>
-        <div class="col-12 col-sm-3 col-md-6">
-          <button
-            class="btn btn-light btn-small btn-block"
-            @click="$bvModal.show('modal-about')"
-          >
-            Sobre el mapa
-          </button>
-        </div>
-      </div>
-
-      <Adsense :ad-client="adsenseClient" :ad-slot="adsenseSlot"></Adsense>
-
-      <div class="row text-center mt-4">
-        <p class="col-12 col-sm-3 col-md-12 text-muted">
-          Este mapa cuenta con<br />
-          la valiosa colaboración de:
-        </p>
-        <a
-          class="col-6 col-sm-3 col-md-6 col-lg-4"
-          href="https://www.facebook.com/LaCiudadNosRegalaSabores"
-          target="_blank"
-        >
-          <img
-            class="img-fluid"
-            src="../assets/colaborador-lcnrs.png"
-            alt="La ciudad nos regala sabores"
-          />
-        </a>
-        <a
-          class="col-6 col-sm-3 col-md-6 col-lg-4"
-          href="https://www.facebook.com/elrenacerdelalaguna/"
-          target="_blank"
-        >
-          <img
-            class="img-fluid"
-            src="../assets/colaborador-laguna-fvet.png"
-            alt="El Renacer de la Laguna - FVET - UBA"
-          />
-        </a>
-        <a
-          class="col-12 col-sm-3 col-md-12 col-lg-4"
-          href="https://www.facebook.com/AsociacionRiberaNorte/"
-          target="_blank"
-        >
-          <img
-            class="img-fluid"
-            src="../assets/colaborador-arn.png"
-            alt="Asociación Ribera Norte"
-          />
-        </a>
-      </div>
-    </aside>
-    <main class="col-md-8 col-lg-9">
-      <!-- TODO: iframe -->
-    </main>
+      </aside>
+      <main class="col-md-8 col-lg-9 p-0">
+        <Map />
+      </main>
+    </div>
     <About />
   </div>
 </template>
@@ -89,6 +91,7 @@
 // Components
 import Form from "@/components/Form.vue";
 import Adsense from "@/components/Adsense.vue";
+import Map from "@/components/Map.vue";
 
 // Modals
 import About from "@/modals/About.vue";
@@ -98,6 +101,7 @@ export default {
   components: {
     Form,
     Adsense,
+    Map,
     About
   },
   data: function() {

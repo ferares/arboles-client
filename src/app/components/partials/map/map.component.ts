3ceffc6b9dd0b0e9da6cf8144b4acfd2a3adb82c
Fermin <fermin@ares.uy>
2020-10-06 12:04:02 -0300

    Set marker on map

--- src/app/components/partials/map/map.component.ts
+++ src/app/components/partials/map/map.component.ts
@@ -33,7 +33,47 @@ export class MapComponent {
     minZoom: 5,
     zoom: 12,
   };
+  public marker: L.Marker;
+  public circle: L.Circle;
   public trees: L.Marker[] = [];
 
   constructor() { }
+
+  public setMarker(event): void {
+    const map = event.target;
+    // If there's no marker on the map...
+    if (!this.marker) {
+      L.Icon.Default.imagePath = 'assets/imgs/markers/';
+      // Create a new marker
+      this.marker = new L.Marker([event.latlng.lat, event.latlng.lng], {
+        draggable: true,
+        riseOnHover: true,
+      }).addTo(map);
+
+      // Create a circle around it to show the search radius
+      this.circle = new L.Circle(
+        [event.latlng.lat, event.latlng.lng],
+        environment.searchRadius,
+        {
+          color: '#000',
+          fillColor: '#ddd',
+          fillOpacity: 0.3,
+        },
+      ).addTo(map);
+
+      // When the marker is dragged move the circle to it
+      this.marker.on('dragend', (dragEvent) => {
+        const latlng = dragEvent.target.getLatLng();
+        this.circle.setLatLng(latlng);
+        map.panTo(new L.LatLng(latlng.lat, latlng.lng));
+      });
+    } else {
+      // If a marker already exists, move it and its circle
+      this.marker.setLatLng([event.latlng.lat, event.latlng.lng]);
+      this.circle.setLatLng([event.latlng.lat, event.latlng.lng]);
+    }
+
+    // Re-center the map around the marker
+    map.panTo(new L.LatLng(event.latlng.lat, event.latlng.lng));
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,7 @@ export const environment = {
   mapDisableClusteringAt: 21,
   mapboxToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
   production: false,
+  searchRadius: 1000,
 };
 
 /*
